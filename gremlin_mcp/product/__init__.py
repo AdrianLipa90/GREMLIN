39bb30e6afab401f2064c72460d31cc0246ec6e1
Adrian Lipa <adrianlipa90@gmail.com>
2026-08-30 10:25:54 +0100

    feat(product): export compact license key helpers

--- gremlin_mcp/product/__init__.py
+++ gremlin_mcp/product/__init__.py
@@ -1,6 +1,7 @@
 """GREMLIN product licensing and client-profile layer."""
 
 from .gate import ProductAuthorizationError, ProductRuntime
+from .keycodec import LICENSE_KEY_PREFIX, decode_license_key, encode_license_key, verify_license_key
 from .license import (
     LICENSE_ENVELOPE_SCHEMA,
     LICENSE_PAYLOAD_SCHEMA,
@@ -14,13 +15,17 @@ from .profile import CLIENT_PROFILE_SCHEMA, ClientProfileError, load_client_prof
 __all__ = [
     "CLIENT_PROFILE_SCHEMA",
     "LICENSE_ENVELOPE_SCHEMA",
+    "LICENSE_KEY_PREFIX",
     "LICENSE_PAYLOAD_SCHEMA",
     "ClientProfileError",
     "LicenseError",
     "ProductAuthorizationError",
     "ProductRuntime",
+    "decode_license_key",
+    "encode_license_key",
     "issue_license",
     "load_client_profile",
     "load_license",
     "verify_license",
+    "verify_license_key",
 ]
